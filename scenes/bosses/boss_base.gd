b4eeee0ce6f116cd489fbd8e68b4c1484f2df141
FraMaffezzoli <francesco.maffezzoli@gmail.com>
2025-01-18 10:22:44 +0100

    added boss folder and blueprint

---
+++ scenes/bosses/boss_base.gd
@@ -0,0 +1,16 @@
+extends Node2D
+class_name BossBase
+
+@export var hp: float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func on_hit() -> void:
+	pass
