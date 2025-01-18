95657468763884f38d6e4337228666e06f61cd27
Oscar Pindaro <oscar.pindaro@gmail.com>
2025-01-18 12:55:03 +0100

    added interaction and damage logic on stomp

--- scenes/bosses/strongman/stomp_attack.gd
+++ scenes/bosses/strongman/stomp_attack.gd
@@ -5,6 +5,9 @@ extends Node2D
 @onready var interaction_area: Area2D = $InteractionArea
 @onready var collision_shape: CollisionShape2D = $InteractionArea/InteractionShape
 
+@onready var hurt_box: Area2D = $HurtBox
+@onready var stomp_sprite: Sprite2D = $StompSprite
+
 @export var interaction_radius : float :
 	set(value):
 		interaction_radius = float(value)
@@ -13,12 +16,59 @@ extends Node2D
 			print("aa")
 			collision_shape.shape.radius = interaction_radius
 
+
+var enabled: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if collision_shape == null:
-		collision_shape = $InteractionArea/InteractionShape
-
+	if not Engine.is_editor_hint():
+		# Code to execute in game.
+		interaction_area.body_entered.connect(on_player_entered_in_interaction)
+		interaction_area.body_exited.connect(on_player_exited_in_interaction)
+		hurt_box.body_entered.connect(on_player_entered_hurt_box)
+		hurt_box.body_entered.connect(on_player_exited_hurt_box)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func run() -> void:
+	if not Engine.is_editor_hint():
+		# Code to execute in game.
+		enabled = true
+		stomp_sprite.visible = true
+		set_collisions(true)
+
+func stop() -> void:
+	if not Engine.is_editor_hint():
+		# Code to execute in game.
+		enabled = false
+		stomp_sprite.visible = false
+		set_collisions(false)
+
+func set_collisions(value: bool):
+	$HurtBox/CollisionShape2D.disabled = not value
+	$InteractionArea/InteractionShape.disabled = not value
+
+func on_player_entered_in_interaction(body: Node2D):
+	if body.is_in_group("Player"):
+		# add a cast
+		var player = body as Player
+		# probably i need to give the player the position in their coordinate space
+		player.pull(self.position)
+
+func on_player_exited_in_interaction(body: Node2D):
+	if body.is_in_group("Player"):
+		# add a cast
+		var player = body as Player
+
+func on_player_entered_hurt_box(body: Node2D):
+	if body.is_in_group("Player"):
+		# add a cast
+		var player = body as Player
+		player.take_damage(1.)
+
+func on_player_exited_hurt_box(body: Node2D):
+	if body.is_in_group("Player"):
+		# add a cast
+		var player = body as Player
