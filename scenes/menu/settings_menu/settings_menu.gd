9a8c03d90247498603c3fe075b1632e2979fccbf
Franco Forte <samuele.loiacono@gmail.com>
2025-01-18 22:57:15 +0100

    Aggiunta gestione end screen

--- scenes/menu/settings_menu/settings_menu.gd
+++ scenes/menu/settings_menu/settings_menu.gd
@@ -1,10 +1,10 @@
-extends Control
+extends BaseMenu
 signal settings_switched
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#print_tree()
-	#_switch_visibility(false)
+	_switch_visibility(false)
 	#print(visible)
 	pass
 
@@ -24,3 +24,7 @@ func _switch_visibility(visible : bool):
 	var children = get_children()
 	for child in children:
 		child.visible = visible
+		
+func _on_next_scene(game_scene: PackedScene) -> void:	
+	super._on_next_scene(game_scene)
+	_on_settings_switched()
