a985bd66dd362438ee0673d424a436dcf50e6d2f
FabioLosavio <fabio.losavio@hotmail.it>
2026-05-10 23:44:39 +0200

    implemented show/hide logic if touch screen is enabled, changed button positioning

--- addons/WAUCommands/mobile_ui/mobile_ui.gd
+++ addons/WAUCommands/mobile_ui/mobile_ui.gd
@@ -2,11 +2,19 @@ extends Control
 
 @onready var d_pad: Control = $CanvasLayer/DPad
 @onready var ui_button = preload("res://addons/WAUCommands/mobile_ui/mobile_ui_button.tscn")
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
 
 var dpad_actions = ["wau_up", "wau_left", "wau_down", "wau_right"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var is_mobile_platform = DisplayServer.is_touchscreen_available()
+	if is_mobile_platform:
+		self.show_all()
+		print("on mobile")
+	else:
+		self.hide_all()
+		print("not on mobile")
 	
 	# Extract all the custom commands set up
 	var custom_commands: Dictionary = {}
@@ -16,13 +24,9 @@ func _ready() -> void:
 			custom_commands[action] = []
 			
 	# build the DPAD
-	#custom_commands.erase("wau_up")
-	#custom_commands.erase("wau_down")
-	#custom_commands.erase("wau_left")
-	#custom_commands.erase("wau_right")
-	
+	var ui_button_size = ui_button.instantiate().size
 	var dpad_center = d_pad.size / 2.0
-	var radius = min(d_pad.size.x, d_pad.size.y) / 2.0 - d_pad.size.x/4  # margin for button size
+	var radius = min(d_pad.size.x, d_pad.size.y) / 2.0 - max(ui_button_size.x, ui_button_size.y) / 2.0  # margin for button size
 	
 	var d_pad_buttons_positions = arrange_in_circle(len(dpad_actions), radius, dpad_center)
 	
@@ -33,6 +37,8 @@ func _ready() -> void:
 		new_button.text = dpad_actions[i]
 		d_pad.add_child(new_button)
 		new_button.position = pos - new_button.size / 2
+		
+		custom_commands.erase(dpad_actions[i])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -51,3 +57,12 @@ func arrange_in_circle(num_objects: int, radius: float, center=Vector2.ZERO, sta
 		var pos = radius * Vector2.from_angle(start_offset + i * offset)
 		output.push_front(pos + center)
 	return output
+
+
+func show_all():
+	self.show()
+	canvas_layer.show()
+	
+func hide_all():
+	self.hide()
+	canvas_layer.hide()
